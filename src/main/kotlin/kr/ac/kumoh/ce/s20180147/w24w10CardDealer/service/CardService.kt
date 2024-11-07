fa4cc0a39484af5016cdb4345addaebf675a7546
ZeppIine <kim0904min@gmail.com>
2024-11-07 14:29:25 +0900

    하드 코딩 상수화

--- src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w10CardDealer/service/CardService.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w10CardDealer/service/CardService.kt
@@ -7,12 +7,15 @@ import kotlin.random.Random
 
 @Service
 class CardService(private val repository: CardRepository) {
+  companion object {
+    const val CARDS_COUNT = 5
+  }
 
   fun getAllCards(): List<Card> {
     return repository.getAllCards() // 모든 카드 반환
   }
 
-  fun dealCards(count: Int = 5) {
+  fun dealCards(count: Int = CARDS_COUNT) {
     val suits = arrayOf("clubs", "diamonds", "hearts", "spades")
     val ranks = arrayOf("2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace")
 
@@ -25,9 +28,6 @@ class CardService(private val repository: CardRepository) {
       uniqueCards.add(Card(randomRank, randomSuit))
     }
 
-    // 이 방법은 "10"이 "1"로 시작하므로 다른 숫자보다 먼저 나와서 변경했음
-    //val sortedCards = uniqueCards.toList()
-    //    .sortedWith(compareBy<Card> { it.suit }.thenBy { it.rank })
     val sortedCards = uniqueCards.toList()
       .sortedWith(compareBy({ suits.indexOf(it.suit) }, { ranks.indexOf(it.rank) }))
 
