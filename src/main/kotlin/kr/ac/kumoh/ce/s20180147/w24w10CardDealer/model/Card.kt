3bec11158ad0a905d8be6588533d18df551a6836
ZeppIine <kim0904min@gmail.com>
2024-11-07 14:30:26 +0900

    이미지 카드 대응

--- src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w10CardDealer/model/Card.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w10CardDealer/model/Card.kt
@@ -2,5 +2,12 @@ package kr.ac.kumoh.ce.s20180147.w24w10CardDealer.model
 
 data class Card(val rank: String, val suit: String) {
   val imageName: String
-    get() = "${rank}_of_${suit}.png"
+    get() {
+      val name = "${rank}_of_${suit}"
+      return if (rank in setOf("jack", "queen", "king")) {
+        "${name}2.png"
+      } else {
+        "${name}.png"
+      }
+    }
 }
